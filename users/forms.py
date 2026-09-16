6035a6f69444b1d827ac928d8cd62b30672a001b
MosharafMehedi <mosharafmehedi46@gmail.com>
2026-09-16 16:06:22 +0600

    commit

--- users/forms.py
+++ users/forms.py
@@ -3,11 +3,13 @@ from .models import Student
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class StudentForm(forms.ModelForm):
 
     class Meta:
         model = Student
         fields = ['name', 'email', 'age']
+
 
 class RegisterForm(UserCreationForm):
 
@@ -19,3 +21,8 @@ class RegisterForm(UserCreationForm):
             'password1',
             'password2',
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].help_text = ''
